ab41b8c95064985f602dabd1d0b773cbc68efb57
ridhotadjudin <ridhotadjudin@gmail.com>
2024-09-08 23:52:35 +0700

    initialize<add>: first commit

---
+++ Test1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using NUnit.Framework;
+
+namespace PlaywrightTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+
+    }
+    [Test]
+    public async Task Test1()
+    {
+        //playwright
+        using var playwright = await Playwright.CreateAsync();
+        
+        //browser
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false
+        });
+
+        //page
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("http://www.eaapp.somee.com");
+        await page.ClickAsync("text=Login");
+        await page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = "EAApp.jpg"
+        });
+
+        await page.FillAsync("#UserName", "admin");
+        await page.FillAsync("#Password", "password");
+        await page.ClickAsync("text=Log in");
+
+        var isExist = await page.Locator("text='Employee Details'").IsVisibleAsync();
+        Assert.IsTrue(isExist);
+    }
+}
